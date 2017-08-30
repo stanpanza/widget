29e9a08fe5c0ef3ca550eff78eaf38b8fcb5e125
jason weng <wengjianm@gmail.com>
2017-08-30 19:36:02 +0800

    fix upgrade jquery js bug

--- views/themes/widget/assets/javascripts/widget/widget.js
+++ views/themes/widget/assets/javascripts/widget/widget.js
@@ -1,4 +1,4 @@
-(function (factory) {
+(function(factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as anonymous module.
         define(['jquery'], factory);
@@ -9,22 +9,21 @@
         // Browser globals.
         factory(jQuery);
     }
-})(function ($) {
-
+})(function($) {
     'use strict';
 
-    var $body = $('body');
-    var NAMESPACE = 'qor.widget';
-    var EVENT_ENABLE = 'enable.' + NAMESPACE;
-    var EVENT_DISABLE = 'disable.' + NAMESPACE;
-    var EVENT_CHANGE = 'change.' + NAMESPACE;
-    var EVENT_CLICK = 'click.' + NAMESPACE;
-    var TARGET_WIDGET = 'select[name="QorResource.Widgets"]';
-    var TARGET_WIDGET_KIND = '[name="QorResource.Kind"]';
-    var SELECT_FILTER = '[name="QorResource.Widgets"],[name="QorResource.Template"]';
-    var CLASS_IS_NEW = 'qor-layout__widget-new';
-    var CLASS_FORM_SECTION = '.qor-form-section';
-    var CLASS_FORM_SETTING = '.qor-layout__widget-setting';
+    let $body = $('body'),
+        NAMESPACE = 'qor.widget',
+        EVENT_ENABLE = 'enable.' + NAMESPACE,
+        EVENT_DISABLE = 'disable.' + NAMESPACE,
+        EVENT_CHANGE = 'change.' + NAMESPACE,
+        EVENT_CLICK = 'click.' + NAMESPACE,
+        TARGET_WIDGET = 'select[name="QorResource.Widgets"]',
+        TARGET_WIDGET_KIND = '[name="QorResource.Kind"]',
+        SELECT_FILTER = '[name="QorResource.Widgets"],[name="QorResource.Template"]',
+        CLASS_IS_NEW = 'qor-layout__widget-new',
+        CLASS_FORM_SECTION = '.qor-form-section',
+        CLASS_FORM_SETTING = '.qor-layout__widget-setting';
 
     function QorWidget(element, options) {
         this.$element = $(element);
@@ -35,36 +34,35 @@
     QorWidget.prototype = {
         constructor: QorWidget,
 
-        init: function () {
+        init: function() {
             this.bind();
             this.isNewForm = this.$element.hasClass(CLASS_IS_NEW);
             !this.isNewForm && this.addWidgetSlideout();
             this.initSelect();
         },
 
-        bind: function () {
+        bind: function() {
             this.$element
                 .on(EVENT_CHANGE, 'select', this.change.bind(this))
                 .on(EVENT_CLICK, '.qor-widget__new', this.getFormHtml.bind(this))
                 .on(EVENT_CLICK, '.qor-widget__cancel', this.cancelForm.bind(this));
         },
 
-        unbind: function () {
+        unbind: function() {
             this.$element
                 .off(EVENT_CHANGE, 'select', this.change.bind(this))
                 .off(EVENT_CLICK, '.qor-widget__new', this.getFormHtml.bind(this));
-
         },
 
-        initSelect: function () {
+        initSelect: function() {
             var $element = this.$element,
                 $select = $element.find('select').filter(SELECT_FILTER),
                 $kind = $(TARGET_WIDGET_KIND),
                 HINT_TEMPLATE = '<h2 class="qor-page__tips">' + $element.data('hint') + '</h2>';
 
             $select.closest(CLASS_FORM_SECTION).hide();
-            $select.each(function () {
-                if ($(this).find('option').filter('[value!=""]').size() >= 2) {
+            $select.each(function() {
+                if ($(this).find('option').filter('[value!=""]').length >= 2) {
                     $(this).closest(CLASS_FORM_SECTION).show();
                 }
             });
@@ -72,26 +70,27 @@
             if (!this.isNewForm) {
                 if (!$kind.parent().next('.qor-form-section-rows').children().length) {
                     $kind.parent().next('.qor-form-section-rows').append(HINT_TEMPLATE);
-                    if (!$element.find('.qor-field__label').not($kind.closest('.qor-form-section').find('.qor-field__label')).is(':visible')) {
+                    if (
+                        !$element.find('.qor-field__label').not($kind.closest('.qor-form-section').find('.qor-field__label')).is(':visible')
+                    ) {
                         $kind.closest('.qor-form-section').hide();
                         $element.append(HINT_TEMPLATE).parent().find('.qor-form__actions').remove();
                     }
                 }
             }
-
         },
 
-        addWidgetSlideout: function () {
+        addWidgetSlideout: function() {
             var $select = $(TARGET_WIDGET),
                 tabScopeActive = $body.data('tabScopeActive'),
                 isInSlideout = $('.qor-slideout').is(':visible'),
                 $form = $select.closest('form'),
-                actionUrl = $form.data("action-url") || $form.prop('action'),
+                actionUrl = $form.data('action-url') || $form.prop('action'),
                 separator = actionUrl.indexOf('?') !== -1 ? '&' : '?',
                 url,
                 clickTmpl;
 
-            $select.find('option').each(function () {
+            $select.find('option').each(function() {
                 var $this = $(this),
                     val = $this.val();
 
@@ -109,13 +108,11 @@
                     }
 
                     $select.after(clickTmpl);
-
                 }
-
             });
         },
 
-        change: function (e) {
+        change: function(e) {
             let $target = $(e.target),
                 widgetValue = $target.val(),
                 isInSlideout = $('.qor-slideout').is(':visible'),
@@ -141,7 +138,7 @@
             return false;
         },
 
-        getFormHtml: function (e) {
+        getFormHtml: function(e) {
             let $target = $(e.target).closest('a'),
                 widgetType = $target.data('widget-type'),
                 $element = this.$element,
@@ -168,18 +165,18 @@
             $element.find('.qor-layout__widget-actions').show();
             $target.closest('li').find('.qor-slideout__lists-groupname').show();
 
-            $.get(url, function (html) {
+            $.get(url, function(html) {
                 $title.show();
                 $selector.val(widgetType).closest('.qor-form-section').hide();
                 $setting.html(html).trigger('enable');
-            }).fail(function () {
+            }).fail(function() {
                 window.alert('server error, please try again!');
             });
 
             return false;
         },
 
-        cancelForm: function () {
+        cancelForm: function() {
             let $element = this.$element;
 
             if ($element.closest('.qor-bottomsheets').length) {
@@ -191,7 +188,7 @@
             $element.find(CLASS_FORM_SETTING).html('');
         },
 
-        destroy: function () {
+        destroy: function() {
             this.unbind();
             this.$element.removeData(NAMESPACE);
         }
@@ -199,10 +196,11 @@
 
     QorWidget.DEFAULTS = {};
 
-    QorWidget.TEMPLATE_LOADING = '<div style="text-align: center; margin-top: 30px;"><div class="mdl-spinner mdl-js-spinner is-active qor-layout__bottomsheet-spinner"></div></div>';
+    QorWidget.TEMPLATE_LOADING =
+        '<div style="text-align: center; margin-top: 30px;"><div class="mdl-spinner mdl-js-spinner is-active qor-layout__bottomsheet-spinner"></div></div>';
 
-    QorWidget.plugin = function (options) {
-        return this.each(function () {
+    QorWidget.plugin = function(options) {
+        return this.each(function() {
             let $this = $(this),
                 data = $this.data(NAMESPACE),
                 fn;
@@ -215,20 +213,20 @@
                 $this.data(NAMESPACE, (data = new QorWidget(this, options)));
             }
 
-            if (typeof options === 'string' && $.isFunction(fn = data[options])) {
+            if (typeof options === 'string' && $.isFunction((fn = data[options]))) {
                 fn.apply(data);
             }
         });
     };
 
-    $(function () {
+    $(function() {
         let selector = '[data-toggle="qor.widget"]';
 
         $(document)
-            .on(EVENT_DISABLE, function (e) {
+            .on(EVENT_DISABLE, function(e) {
                 QorWidget.plugin.call($(selector, e.target), 'destroy');
             })
-            .on(EVENT_ENABLE, function (e) {
+            .on(EVENT_ENABLE, function(e) {
                 QorWidget.plugin.call($(selector, e.target));
             })
             .triggerHandler(EVENT_ENABLE);
@@ -239,5 +237,4 @@
     });
 
     return QorWidget;
-
 });
